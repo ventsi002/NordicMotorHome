491b5c8c811e32c9500a5d29a15d0112549ebfa6
ventsi002 <viliev2002@gmail.com>
2025-05-01 02:21:01 +0300

    reupload

---
+++ src/main/java/com/example/nordicmotorhome/model/Customer.java
@@ -0,0 +1,53 @@
+package com.example.nordicmotorhome.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    private String phoneNumber;
+    private String firstName, lastName;
+    private String username;
+
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+}
---
+++ src/main/java/com/example/nordicmotorhome/repository/MotorhomeRepo.java
@@ -0,0 +1,35 @@
+package com.example.nordicmotorhome.repository;
+
+import com.example.nordicmotorhome.model.Motorhome;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MotorhomeRepo
+{
+    @Autowired
+    JdbcTemplate template;
+
+    public List<Motorhome> fetchNotAvailable(String startDate, String endDate)
+    {
+        String sql = "SELECT heroku_4aa3497124398a6.vehicle.LicenseNumber FROM heroku_4aa3497124398a6.vehicle, heroku_4aa3497124398a6.bookings WHERE heroku_4aa3497124398a6.vehicle.LicenseNumber = heroku_4aa3497124398a6.bookings.LicenseNumber AND (? BETWEEN StartDate AND EndDate OR ? BETWEEN StartDate AND EndDate)";
+        RowMapper<Motorhome> rowMapper  = new BeanPropertyRowMapper<>(Motorhome.class);
+        return template.query(sql, rowMapper, startDate, endDate);
+    }
+    
+    public List<Motorhome> fetchAll()
+    {
+        String sql = "SELECT * FROM heroku_4aa3497124398a6.vehicle";
+        RowMapper<Motorhome> rowMapper = new BeanPropertyRowMapper<>(Motorhome.class);
+        return template.query(sql, rowMapper);
+    }
+    
+
+}
